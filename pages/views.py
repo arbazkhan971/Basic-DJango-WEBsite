c984ea02bd162a13bda29a906bcc35eb5acf216e
Arbaz Khan <arbazkhan971@gmail.com>
2019-03-22 02:16:52 +0530

    Final changes:basic website is ready

--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,8 @@ def home(request):
 	return render(request,"home.html",{})
 
 def about(request):
-	return render(request,"about.html",{})
+	name = "HEllo world my name is arbaz khan"
+	from pages.namer import namer
+	return render(request,"about.html",{"name" : namer })
 def contact(request):
 	return render(request,"contact.html",{})
